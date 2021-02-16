9afd6937d843754020ba30e89fbefe17186ec71e
rochelle-scott <rochellescott.dev@gmail.com>
2021-02-16 14:49:06 +0000

    Created an textField for the new item alert.

--- SmartList/AppDelegate.swift
+++ SmartList/AppDelegate.swift
@@ -1,9 +1,8 @@
 //
 //  AppDelegate.swift
-//  Destini
+//  SmartList App
 //
-//  Created by Philipp Muellauer on 01/09/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//  Created by Rochelle Scott on 16/02/2021.
 //
 
 import UIKit
@@ -16,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("didFinishLaunchingWithOptions")
+        
         return true
     }
 
@@ -27,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("applicationDidEnterBackground")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -38,7 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Called when the application is about to terminate.
+        print("applicationWillTerminate")
     }
 
 
